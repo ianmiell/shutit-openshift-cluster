618410f1bbb3d9caac5b562fdde1234088f00842
Ian Miell <ian.miell@gmail.com>
2018-10-04 11:08:42 +0100

    tables

--- library/controller.py
+++ library/controller.py
@@ -4,8 +4,8 @@ def do_controller(s):
 	s.send('mv go /usr/local')
 	s.send('mkdir /root/go')
 	s.send('export GOROOT=/usr/local/go')
-    s.send('export GOPATH=/root/go')
-    s.send('export PATH=$GOPATH/bin:$GOROOT/bin:$PATH')
+	s.send('export GOPATH=/root/go')
+	s.send('export PATH=$GOPATH/bin:$GOROOT/bin:$PATH')
 	s.send('''cat >> /root/.profile << END
 export GOROOT=/usr/local/go
 export GOPATH=/root/go
