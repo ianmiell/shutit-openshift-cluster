fc75b5e9edbc454d0fedffb1c4813338214b712d
Ian Miell <ian.miell@gmail.com>
2018-10-09 14:09:47 +0100

    logging

--- library/check_nodes.py
+++ library/check_nodes.py
@@ -1,13 +1,16 @@
+import logging
 def check_nodes(shutit_master1_session, test_config_module, vagrantcommand, vagrant_provider, pw):
 	# 1) CHECK NODES COME UP
 	shutit_master1_session.send_until('oc --config=/etc/origin/master/admin.kubeconfig get all || tail /tmp/chef.log*','.*kubernetes.*',cadence=60,note='Wait until oc get all returns OK')
 	for machine in test_config_module.machines.keys():
 		if test_config_module.machines[machine]['is_node']:
 			while True:
-				output = shutit_master1_session.send_and_get_output('oc --config=/etc/origin/master/admin.kubeconfig get nodes')
+				output = shutit_master1_session.send_and_get_output('oc --config=/etc/origin/master/admin.kubeconfig get nodes | grep ' + machine)
 				if output.find(' Ready') != -1:
 					break
 				shutit_master1_session.send('sleep 10')
+		else:
+			shutit_master1_session.log(machine + ' is not a node?', level=logging.WARNING)
 
 
 def label_nodes(shutit_master1_session, test_config_module):
