459a69e0859b36c9820e3ffe4f7f123f9ef0a3ca
Ian Miell <ian.miell@gmail.com>
2018-10-10 07:46:05 +0100

    reduce sleep

--- shutit_openshift_cluster.py
+++ shutit_openshift_cluster.py
@@ -394,7 +394,7 @@ cookbook_path            ["#{current_dir}/../cookbooks"]'''
 		check_nodes.check_nodes(shutit_master1_session, test_config_module, vagrantcommand, vagrant_provider, pw)
 		shutit_master1_session.send('systemctl stop crond')
 		# This pause appears to be needed to ensure things settle down. Otherwise it seems that router and registry may die without leaving any obvious trace.
-		shutit_master1_session.send('sleep 600')
+		shutit_master1_session.send('sleep 60')
 		check_nodes.label_nodes(shutit_master1_session, test_config_module)
 
 		run_apps.do_run_apps(test_config_module, shutit_master1_session, shutit, shutit_session)
