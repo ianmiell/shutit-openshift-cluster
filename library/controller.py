a77a0f830c5b63738be68286853bbfbcb40915a7
Ian Miell <ian.miell@gmail.com>
2018-10-19 09:59:19 +0100

    sc

--- library/controller.py
+++ library/controller.py
@@ -2,8 +2,7 @@ def do_controller(s):
 	s.send('go version')
 	s.send('go env')
 	s.send('go get k8s.io/sample-controller')
-	s.send('go get github.com/kubernetes/sample-controller')
-	s.send('cd $GOROOT/src/k8s.io/sample-controller')
+	s.send('cd /root/go/src/k8s.io/sample-controller')
 	# Build crd
 	s.send('kubectl apply -f artifacts/examples/crd-validation.yaml')
 	# Build the controller.
