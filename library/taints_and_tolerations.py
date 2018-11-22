a9be3f0d305422106f8df7339e238ce48a5f11a0
Ian Miell <ian.miell@gmail.com>
2018-11-22 08:05:52 +0000

    )

--- library/taints_and_tolerations.py
+++ library/taints_and_tolerations.py
@@ -6,6 +6,6 @@ def do_taints_and_tolerations_example(s):
 	# TODO: create a specific namespace
 	s.send('kubectl taint nodes node1 dedicated=postgresql:NoSchedule')
 	# https://docs.openshift.com/enterprise/3.1/using_images/db_images/postgresql.html
-	s.send('oc new-app -e POSTGRESQL_USER=<username>,POSTGRESQL_PASSWORD=postgresql,POSTGRESQL_DATABASE=mydb registry.access.redhat.com/rhscl/postgresql-94-rhel7'
+	s.send('oc new-app -e POSTGRESQL_USER=<username>,POSTGRESQL_PASSWORD=postgresql,POSTGRESQL_DATABASE=mydb registry.access.redhat.com/rhscl/postgresql-94-rhel7')
 	# TODO: edit the pod, and check node1 status
 	s.pause_point('# TODO: edit the pod, and check node1 status')
