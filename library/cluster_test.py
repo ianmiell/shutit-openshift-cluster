d4d3d2c8e5370114c47d3d2061cc79d830183c68
Ian Miell <ian.miell@gmail.com>
2019-01-23 12:52:30 +0000

    abstracted

--- library/cluster_test.py
+++ library/cluster_test.py
@@ -20,7 +20,6 @@ def test_cluster(shutit, shutit_sessions, shutit_master1_session, test_config_mo
 		shutit_session.send('oc adm new-project mysql')
 	ok = False
 	while not ok:
-		count = 80
 		shutit.log('Iterations left: ' + str(count),level=logging.INFO)
 		# Destroy all...
 		shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig delete svc mysql -n mysql', check_exit=False)
@@ -29,32 +28,8 @@ def test_cluster(shutit, shutit_sessions, shutit_master1_session, test_config_mo
 		shutit_session.send('''oc --config=/etc/origin/master/admin.kubeconfig get pods -n mysql | grep mysql | awk '{print $1}' | xargs -n1 oc --config=/etc/origin/master/admin.kubeconfig delete pod -n mysql || true''')
 		# --allow-missing-images has been seen to be needed very occasionally.
 		shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig new-app -e=MYSQL_ROOT_PASSWORD=root mysql --allow-missing-images -n mysql')
-		# TODO: call check_app
-		while True:
-			if count == 0:
-				break
-			count -= 1
-			# Sometimes terminating containers don't go away quickly.
-			status = shutit_session.send_and_get_output("""oc --config=/etc/origin/master/admin.kubeconfig get pods -n mysql | grep ^mysql- | grep -v deploy | awk '{print $3}' | grep -v Terminating | head -1""")
-			if status == 'Running':
-				ok = True
-				break
-			elif status == 'Error':
-				break
-			elif status == 'ImagePullBackOff':
-				shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig deploy mysql --cancel -n mysql || oc --config=/etc/origin/master/admin.kubeconfig  rollout cancel dc/mysql -n mysql')
-				shutit_session.send('sleep 15')
-				shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig deploy mysql --retry -n mysql || oc --config=/etc/origin/master/admin.kubeconfig deploy mysql --latest -n mysql || oc --config=/etc/origin/master/admin.kubeconfig rollout retry dc/mysql -n mysql || oc --config=/etc/origin/master/admin.kubeconfig rollout latest dc/mysql -n mysql')
-			# Check on deployment
-			deploy_status = shutit_session.send_and_get_output("""oc --config=/etc/origin/master/admin.kubeconfig get pods -n mysql | grep ^mysql- | grep -w deploy | awk '{print $3}' | grep -v Terminating | head -1""")
-			# If deploy has errored...
-			if deploy_status == 'Error':
-				# Try and rollout latest, ensure it's been cancelled and roll out again.
-				shutit_session.send('oc rollout cancel dc/mysql -n mysql', check_exit=False)
-				shutit_session.send('oc rollout latest dc/mysql -n mysql')
-			# For debug/info purposes.
-			shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig get pods -n mysql | grep ^mysql',check_exit=False)
-			shutit_session.send('sleep 15')
+		if not check_app('mysql','mysql', '80', '15'):
+			shutit_session.pause_point('mysql app did not start correctly')
 	podname = shutit_session.send_and_get_output("""oc --config=/etc/origin/master/admin.kubeconfig get pods -n mysql | grep mysql | grep -v deploy | awk '{print $1}' | tail -1""")
 	time.sleep(30) # pause to allow resolve to work ok
 	# exec and check hosts google.com and kubernetes.default.svc.cluster.local
@@ -87,7 +62,8 @@ def diagnostic_tests(shutit_session):
 
 
 
-def check_app(namespace, appname):
+def check_app(namespace, appname, iters, sleep):
+	count = iters
 	while True:
 		if count == 0:
 			break
@@ -96,7 +72,7 @@ def check_app(namespace, appname):
 		status = shutit_session.send_and_get_output("""oc --config=/etc/origin/master/admin.kubeconfig get pods -n """ + namespace + """ | grep ^""" + appname + """- | grep -v deploy | awk '{print $3}' | grep -v Terminating | head -1""")
 		if status == 'Running':
 			ok = True
-			break
+			return True
 		elif status == 'Error':
 			break
 		elif status == 'ImagePullBackOff':
@@ -112,5 +88,5 @@ def check_app(namespace, appname):
 			shutit_session.send('oc rollout latest dc/' + appname + ' -n ' + namespace)
 		# For debug/info purposes.
 		shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig get pods -n ' + namespace + ' | grep ^' + appname + '-',check_exit=False)
-		shutit_session.send('sleep 15')
-
+		shutit_session.send('sleep ' + sleep)
+	return False
