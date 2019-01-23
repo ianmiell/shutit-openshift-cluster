3e2351df3bc96a02ea93367233c64d58f2348714
Ian Miell <ian.miell@gmail.com>
2019-01-23 07:18:33 +0000

    net-tools

--- library/cluster_test.py
+++ library/cluster_test.py
@@ -29,7 +29,6 @@ def test_cluster(shutit, shutit_sessions, shutit_master1_session, test_config_mo
 		shutit_session.send('''oc --config=/etc/origin/master/admin.kubeconfig get pods -n mysql | grep mysql | awk '{print $1}' | xargs -n1 oc --config=/etc/origin/master/admin.kubeconfig delete pod -n mysql || true''')
 		# --allow-missing-images has been seen to be needed very occasionally.
 		shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig new-app -e=MYSQL_ROOT_PASSWORD=root mysql --allow-missing-images -n mysql')
-		# TODO: call check_app
 		while True:
 			if count == 0:
 				break
@@ -56,12 +55,50 @@ def test_cluster(shutit, shutit_sessions, shutit_master1_session, test_config_mo
 			shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig get pods -n mysql | grep ^mysql',check_exit=False)
 			shutit_session.send('sleep 15')
 	podname = shutit_session.send_and_get_output("""oc --config=/etc/origin/master/admin.kubeconfig get pods -n mysql | grep mysql | grep -v deploy | awk '{print $1}' | tail -1""")
-	time.sleep(30) # pause to allow resolve to work ok
 	# exec and check hosts google.com and kubernetes.default.svc.cluster.local
 	# ping has been removed!
-	shutit_session.send("""oc --config=/etc/origin/master/admin.kubeconfig -n mysql exec -ti """ + podname + """ -- /bin/sh -c 'resolveip google.com'""")
+	if shutit_session.send_and_get_output('oc get projects | grep net | wc -l') == '0':
+		shutit_session.send('oc adm new-project net')
+	while not ok:
+		count = 80
+		shutit.log('Iterations left: ' + str(count),level=logging.INFO)
+		# Destroy all...
+		shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig delete svc net-tools -n net', check_exit=False)
+		shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig delete dc net-tools -n net', check_exit=False)
+		shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig delete is net-tools -n net', check_exit=False)
+		shutit_session.send('''oc --config=/etc/origin/master/admin.kubeconfig get pods -n net | grep net-tools | awk '{print $1}' | xargs -n1 oc --config=/etc/origin/master/admin.kubeconfig delete pod -n net || true''')
+		# --allow-missing-images has been seen to be needed very occasionally.
+		shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig new-app imiell/net-tools --allow-missing-images -n net')
+		while True:
+			if count == 0:
+				break
+			count -= 1
+			# Sometimes terminating containers don't go away quickly.
+			status = shutit_session.send_and_get_output("""oc --config=/etc/origin/master/admin.kubeconfig get pods -n net | grep ^net-tools | grep -v deploy | awk '{print $3}' | grep -v Terminating | head -1""")
+			if status == 'Running':
+				ok = True
+				break
+			elif status == 'Error':
+				break
+			elif status == 'ImagePullBackOff':
+				shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig deploy net-tools --cancel -n net || oc --config=/etc/origin/master/admin.kubeconfig  rollout cancel dc/net-tools -n net')
+				shutit_session.send('sleep 15')
+				shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig deploy net-tools --retry -n net || oc --config=/etc/origin/master/admin.kubeconfig deploy net-tools --latest -n net || oc --config=/etc/origin/master/admin.kubeconfig rollout retry dc/net-tools -n net || oc --config=/etc/origin/master/admin.kubeconfig rollout latest dc/net-tools -n net')
+			# Check on deployment
+			deploy_status = shutit_session.send_and_get_output("""oc --config=/etc/origin/master/admin.kubeconfig get pods -n net | grep ^net-tools | grep -w deploy | awk '{print $3}' | grep -v Terminating | head -1""")
+			# If deploy has errored...
+			if deploy_status == 'Error':
+				# Try and rollout latest, ensure it's been cancelled and roll out again.
+				shutit_session.send('oc rollout cancel dc/net-tools -n net', check_exit=False)
+				shutit_session.send('oc rollout latest dc/net-tools -n net')
+			# For debug/info purposes.
+			shutit_session.send('oc --config=/etc/origin/master/admin.kubeconfig get pods -n net | grep ^net-tools',check_exit=False)
+			shutit_session.send('sleep 15')
+	time.sleep(30) # pause to allow resolve to work ok
+	podname = shutit_session.send_and_get_output("""oc --config=/etc/origin/master/admin.kubeconfig get pods -n net | grep net-tools | grep -v deploy | awk '{print $1}' | tail -1""")
+	shutit_session.send("""oc --config=/etc/origin/master/admin.kubeconfig -n net exec -ti """ + podname + """ -- /bin/sh -c 'resolveip google.com'""")
 	for addr in ('kubernetes.default.svc','kubernetes.default.svc.cluster.local'):
-		if shutit_session.send_and_get_output("""oc --config=/etc/origin/master/admin.kubeconfig -n mysql exec -ti """ + podname + """ -- /bin/sh -c 'resolveip """ + addr + """ -s'""") != '172.30.0.1':
+		if shutit_session.send_and_get_output("""oc --config=/etc/origin/master/admin.kubeconfig -n net-tools exec -ti """ + podname + """ -- /bin/sh -c 'resolveip """ + addr + """ -s'""") != '172.30.0.1':
 			shutit_session.pause_point('kubernetes.default.svc.cluster.local did not resolve correctly')
 
 
