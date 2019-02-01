e5c703dc04fa1213005dcbf9638b16fbb734d2cc
ian <ian.miell@gmail.com>
2019-02-01 14:22:52 +0000

    ws

--- library/shared.py
+++ library/shared.py
@@ -2,6 +2,6 @@
 # For 3.7 upgrade, docker upgrade requires a redeploy to ensure all is ok
 def redeploy_components(shutit_session,apptype='both'):
 	if apptype in ('both','registry'):
-        shutit_session.send('oc rollout latest docker-registry')
+		shutit_session.send('oc rollout latest docker-registry')
 	if apptype in ('both','router'):
-        shutit_session.send('oc rollout latest router')
+		shutit_session.send('oc rollout latest router')
