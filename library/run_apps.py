09e5b9c63dfa085721f92199b9a7ecd136cf9cfa
Ian Miell <ian.miell@gmail.com>
2019-02-06 13:21:52 +0000

    quieter

--- library/run_apps.py
+++ library/run_apps.py
@@ -5,7 +5,7 @@ from library import shared
 def do_run_apps(test_config_module, shutit_master1_session, shutit, shutit_session):
 	while True:
 		ok = False
-		count = 60
+		count = 30
 		shutit.log('Iterations left: ' + str(count),level=logging.INFO)
 		while True:
 			status = shutit_master1_session.send_and_get_output("""oc --config=/etc/origin/master/admin.kubeconfig get pods | grep ^router- | grep -v deploy | awk '{print $3}' | grep -v Terminating | head -1""")
@@ -23,19 +23,17 @@ def do_run_apps(test_config_module, shutit_master1_session, shutit, shutit_sessi
 				shutit_master1_session.send('oc --config=/etc/origin/master/admin.kubeconfig deploy router --retry || oc --config=/etc/origin/master/admin.kubeconfig deploy router --latest || oc --config=/etc/origin/master/admin.kubeconfig rollout retry dc/router || oc --config=/etc/origin/master/admin.kubeconfig rollout latest dc/router')
 			else:
 				shutit_master1_session.send('sleep 15')
-				shutit_master1_session.send('oc --config=/etc/origin/master/admin.kubeconfig describe pods || true')
-				shutit_master1_session.send('oc --config=/etc/origin/master/admin.kubeconfig get all || true')
 		shutit.log('router while loop done.')
+		shutit_master1_session.send('oc --config=/etc/origin/master/admin.kubeconfig describe pods || true')
+		shutit_master1_session.send('oc --config=/etc/origin/master/admin.kubeconfig get all || true')
 		if ok:
 			shutit.log('Broken out of outer loop, router should now be OK.')
 			break
-		shutit_master1_session.send("""oc --config=/etc/origin/master/admin.kubeconfig get pods | grep -w ^router | awk '{print $1}' | xargs oc --config=/etc/origin/master/admin.kubeconfig delete pod || true""")
-		shutit_master1_session.send('oc --config=/etc/origin/master/admin.kubeconfig deploy router --retry || oc --config=/etc/origin/master/admin.kubeconfig deploy router --latest || oc --config=/etc/origin/master/admin.kubeconfig rollout retry dc/router || oc --config=/etc/origin/master/admin.kubeconfig rollout latest dc/router')
 		check_nodes.schedule_nodes(test_config_module, shutit_master1_session)
 		shared.redeploy_components(shutit_master1_session,'router')
 	while True:
 		ok = False
-		count = 20
+		count = 30
 		shutit.log('Iterations left: ' + str(count),level=logging.INFO)
 		while True:
 			# Either registry or docker-registry (more recently?)
@@ -49,9 +47,8 @@ def do_run_apps(test_config_module, shutit_master1_session, shutit, shutit_sessi
 				ok = True
 				break
 			if status in ('Error','ImagePullBackOff'):
-				shutit_master1_session.send('oc --config=/etc/origin/master/admin.kubeconfig deploy docker-registry --cancel || oc --config=/etc/origin/master/admin.kubeconfig rollout cancel dc/docker-registry')
 				shutit_master1_session.send('sleep 14')
-				shutit_master1_session.send('oc --config=/etc/origin/master/admin.kubeconfig deploy docker-registry --retry || oc --config=/etc/origin/master/admin.kubeconfig deploy docker-registry --latest || oc --config=/etc/origin/master/admin.kubeconfig rollout retry dc/docker-registry || oc --config=/etc/origin/master/admin.kubeconfig rollout latest dc/docker-registry')
+				shared.redeploy_components(shutit_master1_session,'registry')
 			else:
 				shutit_master1_session.send('sleep 14')
 				deploy_status = shutit_master1_session.send_and_get_output("""oc --config=/etc/origin/master/admin.kubeconfig get pods | grep ^registry- | grep -w deploy | awk '{print $3}' | grep -v Terminating | head -1""")
@@ -63,7 +60,5 @@ def do_run_apps(test_config_module, shutit_master1_session, shutit, shutit_sessi
 			break
 		else:
 			# Try and rectify
-			shutit_master1_session.send("""oc --config=/etc/origin/master/admin.kubeconfig get pods | grep -w registry | awk '{print $1}' | xargs oc --config=/etc/origin/master/admin.kubeconfig delete pod || true""")
-			shutit_master1_session.send('oc --config=/etc/origin/master/admin.kubeconfig deploy docker-registry --retry || oc --config=/etc/origin/master/admin.kubeconfig deploy docker-registry --latest || oc --config=/etc/origin/master/admin.kubeconfig rollout retry dc/docker-registry || oc --config=/etc/origin/master/admin.kubeconfig rollout latest dc/docker-registry')
 			check_nodes.schedule_nodes(test_config_module, shutit_master1_session)
 			shared.redeploy_components(shutit_master1_session,'registry')
